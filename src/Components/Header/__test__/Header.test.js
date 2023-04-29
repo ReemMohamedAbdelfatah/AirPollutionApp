c96048f1f0fcc928bc622637133a7830be059e3c
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-04-29 21:01:20 +0300

    solve linter error

--- src/Components/Header/__test__/Header.test.js
+++ src/Components/Header/__test__/Header.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Header from '../Header.js';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Header from '../Header.js';
 import store from '../../../redux/configureStore.js';
 
 describe('Header Component', () => {
   it('matches snapshot', () => {
     const tree = renderer.create(
       <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-      </Provider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Provider>,
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
